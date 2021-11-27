b9bba090688a2c59936632c9eb696b16fee0d3cb
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2021-11-27 07:19:04 -0500

    feat: pages components, home.comp - dashboard.comp

--- admin-dashboard-basic/src/app/app.module.ts
+++ admin-dashboard-basic/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
