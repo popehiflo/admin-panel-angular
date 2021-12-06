d5872c5cb6d4ed12bce2432256a03085a2dd964a
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2021-12-06 09:14:44 -0500

    refact: breadcrumb.comp, modifica cada page

--- admin-dashboard-basic/src/app/shared/breadcrumb/breadcrumb.component.ts
+++ admin-dashboard-basic/src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbComponent implements OnInit {
 
+  title:string = 'Breadcrumb';
+
   constructor() { }
 
   ngOnInit(): void {
